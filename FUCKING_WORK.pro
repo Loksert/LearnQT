05fbbda069dffd588812c3a20fb70747b431efb3
Damond LOKS <super.dimasik53@yandex.ru>
2020-04-24 20:28:39 +0300

    Add folders

--- FUCKING_WORK.pro
+++ FUCKING_WORK.pro
@@ -26,12 +26,10 @@ CONFIG += c++11
 
 SOURCES += \
         main.cpp \
-        mainwindow.cpp \
-        syntaxl.cpp
+        mainwindow.cpp
 
 HEADERS += \
-        mainwindow.h \
-        syntaxl.h
+        mainwindow.h
 
 FORMS += \
         mainwindow.ui
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <QtWidgets>
-#include "syntaxl.h"
 
 
 int main (int argc, char** argv)
@@ -13,20 +12,29 @@ int main (int argc, char** argv)
     QStringList llst;
     QStringList lst;
     QTableWidget tbl(n,n);
-    QTableWidgetItem* ptwi = nullptr;
 
     lst<<"First"<<"Second"<<"Third";
     tbl.setHorizontalHeaderLabels(lst);
     tbl.setVerticalHeaderLabels(lst);
-
     tab.addTab(&tbl,"Table");
-    tab.addTab(new QLabel("Linux",&tab),"Linux");
 
-    tbl.resize(370,135);
-    tab.resize(370,135);
+    QSplitter spl(Qt::Horizontal);
+    QFileSystemModel model;
+
+    model.setRootPath(QDir::rootPath());
+    QTreeView* pTreeView = new QTreeView;
+    pTreeView->setModel(&model);
+    QTableView* pTableView = new QTableView;
+    pTableView->setModel(&model);
+
+    QObject::connect(pTreeView,SIGNAL(clicked(const QModelIndex&)),pTableView,SLOT(setRootIndex(const QModelIndex&)));
+    QObject::connect(pTableView,SIGNAL(activated(const QModelIndex&)),pTreeView,SLOT(setCurrentIndex(const QModelIndex&)));
+    QObject::connect(pTableView,SIGNAL(activated(const QModelIndex&)),pTreeView,SLOT(setRootIndex(const QModelIndex&)));
+    spl.addWidget(pTreeView);
+    spl.addWidget(pTableView);
+
+    tab.addTab(&spl,"Folders");
+
     tab.show();
-
-
-
     return app.exec();
 }
